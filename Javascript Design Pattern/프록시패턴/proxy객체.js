f983a8e6a97f526e7e6c70f4d730451e570826db
JunYoungJS <ktsdsd94@gmail.com>
2022-08-07 23:20:52 +0900

    [ADD] Proxy패턴 study

---
+++ Javascript Design Pattern/프록시패턴/proxy객체.js
@@ -0,0 +1,32 @@
+// 프록시 객체를 이용한 유효성 체크  (proxy객체가 왜 좋은지 알아바야할듯)
+
+const person = {
+  name: "John Doe",
+  age: 42,
+  nationality: "American"
+};
+
+const personProxy = new Proxy(person, {
+  get: (obj, prop) => {
+    if (!obj[prop]) {
+      console.log(`Hmm.. this property doesn't seem to exist`);
+    } else {
+      console.log(`The value of ${prop} is ${obj[prop]}`);
+    }
+  },
+  set: (obj, prop, value) => {
+    if (prop === "age" && typeof value !== "number") {
+      console.log(`Sorry, you can only pass numeric values for age.`);
+    } else if (prop === "name" && value.length < 2) {
+      console.log(`You need to provide a valid name.`);
+    } else {
+      console.log(`Changed ${prop} from ${obj[prop]} to ${value}.`);
+      obj[prop] = value;
+    }
+    return true;
+  }
+});
+
+personProxy.nonExistentProperty;
+personProxy.age = "44";
+personProxy.name = "";
