ccdb73711e02c55b0563758e892cc3ade8fe9e74
TheGiantKiller <ktsdsd94@gmail.com>
2023-01-05 16:12:15 +0900

    [Study] Next.js Chapter6

---
+++ Next.js/lecture/Chapter6/Practice/pages/events/[eventId].js
@@ -0,0 +1,57 @@
+import { Fragment } from 'react';
+
+
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+import { getEventById ,getAllEvents, getFeaturedEvents} from '../../helpers/api-util';
+
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
+
+  if (!event) {
+    return (
+      <div className="center">
+        <p>Loading ... </p>
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+
+export async function getStaticProps(context){
+  const eventId=context.params.eventId
+  const event=await getEventById(eventId)
+  return {
+    props:{
+      selectedEvent:event
+    },
+    revalidate:30
+  }
+}
+export async function getStaticPaths(){
+  const events=await getFeaturedEvents()
+  const paths=events.map(event=>({params:{eventId:event.id}}))
+  return {
+    paths:paths,
+    fallback:true
+  }
+}
+
+export default EventDetailPage;
---
+++ Next.js/lecture/Chapter6/Practice/pages/index.js
@@ -0,0 +1,23 @@
+
+import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from '../helpers/api-util';
+
+function HomePage(props) {
+  return (
+    <div>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps(){
+  const featuredEvents=await getFeaturedEvents()
+  return {
+    props:{
+      events:featuredEvents
+    },
+    revalidate:1800
+  }
+}
+
+export default HomePage;
